c2508abcc4be4cf73818ec31c6d902e8cad2f581
blueskyfog0904 <blueskyfog@gmail.com>
2023-07-14 15:13:30 +0900

    실전 스프링데이터JPA 강좌 섹션4 쿼리 메서드 기능 > 메소드 이름으로 쿼리 생성 > JPA NamedQuery

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -7,6 +7,10 @@ import lombok.*;
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"})
+@NamedQuery(
+        name="Member.findByUsername",
+        query="select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id @GeneratedValue
@@ -31,6 +35,11 @@ public class Member {
             changeTeam(team);
         }
 
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
     }
 
     public void changeTeam(Team team) {
